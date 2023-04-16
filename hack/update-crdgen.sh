78f667e58c9ebf8ee8831b6c51a67a14591dbc06
Zechun Chen <zechun.chen@daocloud.io>
2023-04-16 11:07:00 +0800

    Ad charts for fast

--- hack/update-crdgen.sh
+++ hack/update-crdgen.sh
@@ -13,4 +13,4 @@ echo "Generating with controller-gen"
 util::install_tools ${CONTROLLER_GEN_PKG} ${CONTROLLER_GEN_VER} >/dev/null 2>&1
 
 # Unify the crds used by helm chart and the installation scripts
-controller-gen crd paths=./pkg/apis/ips/... output:crd:dir=./charts/ips/_crds/bases
+controller-gen crd paths=./pkg/apis/ips/... output:crd:dir=./charts/fast/_crds/bases
