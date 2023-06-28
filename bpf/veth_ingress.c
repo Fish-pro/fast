5d0440f4fc72a54cd0023822e66c721a41ece494
Zechun Chen <zechun.chen@daocloud.io>
2023-06-28 14:15:50 +0800

    fix bpf program
    Signed-off-by: Zechun Chen <zechun.chen@daocloud.io>

--- bpf/veth_ingress.c
+++ bpf/veth_ingress.c
@@ -11,39 +11,39 @@
 
 __section("classifier")
 int cls_main(struct __sk_buff *skb) {
-	void *data = (void *)(long)skb->data;
-	void *data_end = (void *)(long)skb->data_end;
-	if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
+  void *data = (void *)(long)skb->data;
+  void *data_end = (void *)(long)skb->data_end;
+  if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
     return TC_ACT_UNSPEC;
   }
 
-	struct ethhdr  *eth  = data;
-	struct iphdr   *ip   = (data + sizeof(struct ethhdr));
+  struct ethhdr  *eth  = data;
+  struct iphdr   *ip   = (data + sizeof(struct ethhdr));
   if (eth->h_proto != __constant_htons(ETH_P_IP)) {
 		return TC_ACT_UNSPEC;
   }
 
   __u32 src_ip = htonl(ip->saddr);
-	__u32 dst_ip = htonl(ip->daddr);
+  __u32 dst_ip = htonl(ip->daddr);
   __u8 src_mac[ETH_ALEN];
-	__u8 dst_mac[ETH_ALEN];
+  __u8 dst_mac[ETH_ALEN];
   struct endpointKey epKey = {};
   epKey.ip = dst_ip;
-  struct endpointInfo *ep = bpf_map_lookup_elem(&ding_lxc, &epKey);
+  struct endpointInfo *ep = bpf_map_lookup_elem(&local_pod_ip, &epKey);
   if (ep) {
     bpf_memcpy(src_mac, ep->nodeMac, ETH_ALEN);
-	  bpf_memcpy(dst_mac, ep->mac, ETH_ALEN);
+	bpf_memcpy(dst_mac, ep->mac, ETH_ALEN);
     bpf_skb_store_bytes(skb, offsetof(struct ethhdr, h_source), dst_mac, ETH_ALEN, 0);
-	  bpf_skb_store_bytes(skb, offsetof(struct ethhdr, h_dest), src_mac, ETH_ALEN, 0);
+	bpf_skb_store_bytes(skb, offsetof(struct ethhdr, h_dest), src_mac, ETH_ALEN, 0);
     return bpf_redirect_peer(ep->lxcIfIndex, 0);
   }
   struct podNodeKey podNodeKey = {};
   podNodeKey.ip = dst_ip;
-  struct podNodeValue *podNode = bpf_map_lookup_elem(&ding_ip, &podNodeKey);
+  struct podNodeValue *podNode = bpf_map_lookup_elem(&cluster_pod_ip, &podNodeKey);
   if (podNode) {
     struct localNodeMapKey localKey = {};
     localKey.type = LOCAL_DEV_VXLAN;
-    struct localNodeMapValue *localValue = bpf_map_lookup_elem(&ding_local, &localKey);
+    struct localNodeMapValue *localValue = bpf_map_lookup_elem(&local_dev, &localKey);
     
     if (localValue) {
       return bpf_redirect(localValue->ifIndex, 0);
--- bpf/vxlan_ingress.c
+++ bpf/vxlan_ingress.c
@@ -11,30 +11,30 @@
 __section("classifier")
 int cls_main(struct __sk_buff *skb) {
   void *data = (void *)(long)skb->data;
-	void *data_end = (void *)(long)skb->data_end;
-	if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
+  void *data_end = (void *)(long)skb->data_end;
+  if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
     return TC_ACT_UNSPEC;
   }
 
-	struct ethhdr  *eth  = data;
-	struct iphdr   *ip   = (data + sizeof(struct ethhdr));
+  struct ethhdr  *eth  = data;
+  struct iphdr   *ip   = (data + sizeof(struct ethhdr));
   if (eth->h_proto != __constant_htons(ETH_P_IP)) {
-		return TC_ACT_UNSPEC;
+	return TC_ACT_UNSPEC;
   }
 
   __u32 src_ip = htonl(ip->saddr);
-	__u32 dst_ip = htonl(ip->daddr);
+  __u32 dst_ip = htonl(ip->daddr);
   bpf_printk("the dst_ip is: %d", dst_ip);
   bpf_printk("the ip->daddr is: %d", ip->daddr);
 
   struct endpointKey epKey = {};
   epKey.ip = dst_ip;
-  struct endpointInfo *ep = bpf_map_lookup_elem(&ding_lxc, &epKey);
+  struct endpointInfo *ep = bpf_map_lookup_elem(&local_pod_ip, &epKey);
   if (!ep) {
     return TC_ACT_OK;
   }
   __u8 src_mac[ETH_ALEN];
-	__u8 dst_mac[ETH_ALEN];
+  __u8 dst_mac[ETH_ALEN];
   bpf_memcpy(src_mac, ep->nodeMac, ETH_ALEN);
   bpf_memcpy(dst_mac, ep->mac, ETH_ALEN);
   bpf_skb_store_bytes(
@@ -44,7 +44,7 @@ int cls_main(struct __sk_buff *skb) {
     ETH_ALEN,
     0
   );
-    bpf_skb_store_bytes(
+  bpf_skb_store_bytes(
     skb,
     offsetof(struct ethhdr, h_source),
     src_mac,
